60b55a70c7e3d27e52837cb0321d1b46d7fef9c0
DogeLinux <hexygaming99@gmail.com>
2026-01-02 16:37:42 -0600

    Updated fog top 32 chunks

--- 26.1_shaders/assets/minecraft/shaders/include/fog.glsl
+++ 26.1_shaders/assets/minecraft/shaders/include/fog.glsl
@@ -23,7 +23,8 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
 }
 
 float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd) {
-    return mix(linear_fog_value(sphericalVertexDistance, environmentalStart, environmantalEnd), linear_fog_value(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd), floor(clamp(abs(environmantalEnd / renderDistanceEnd) * 4, 0.0, 1.0)));
+    return mix(linear_fog_value(sphericalVertexDistance, environmentalStart, environmantalEnd), linear_fog_value(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd),
+    floor(clamp(abs(environmantalEnd / renderDistanceEnd) * 8, 0.0, 1.0)));
 }
 
 vec4 apply_fog(vec4 inColor, float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd, vec4 fogColor) {
